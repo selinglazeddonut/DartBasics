7bfe11d3cbe7d97d39e38f4f2ed1a29161afb011
Selin Iscan <sseliniscan@gmail.com>
2024-01-13 23:56:38 +0300

    degiskenler ve veri tipleri

---
+++ lib/dart lessons/ders_1.dart
@@ -0,0 +1,43 @@
+
+/*bool ciftMi(ornek_sayi) {
+  return ornek_sayi %2 ==0;
+}*/
+
+void main() {
+   print("hi");
+
+  var year=2001;
+  if (year>=2024){
+    print("ho ho ho");
+  }
+
+  else if(year<=2024){
+    print("kücük yıl");
+  }
+
+//dart veriyi tanır fakat sayılarda belirt ona göre yer kaplar.
+//var için istediğini atarsın ama işleme baglı.
+ /* var ad = 'emre';
+  print(ad);
+  print(ad + "'nin yası 34");
+
+// int ve double= num degeri
+  num sayi=2.3;
+  print(sayi);
+
+//Eğer bir fonksiyonun döndürdüğü değeri kullanmak istiyorsanız,
+// bu işlem genellikle main fonksiyonu içinde gerçekleşir.
+
+var ornek_sayi=10;
+bool sonuc= ciftMi(ornek_sayi);
+  print(sonuc);
+*/
+
+
+  //direk türünü yazarak da atama yapabilirsin
+  int maas=300000;
+  String isim="Selin";
+  print("ismim $isim");
+
+}
+
